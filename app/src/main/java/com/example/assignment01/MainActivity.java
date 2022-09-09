f4b31a7047ffed75fefbf0357257a44598377b2f
Yuchan-Kim <sksk7799@gmail.com>
2022-09-09 18:15:39 -0400

    Final Commit

--- app/src/main/java/com/example/assignment01/MainActivity.java
+++ app/src/main/java/com/example/assignment01/MainActivity.java
@@ -2,19 +2,31 @@ package com.example.assignment01;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
 public class MainActivity extends AppCompatActivity {
+    TextView text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text1 = findViewById(R.id.textView);
+        text1.setText(" <Text Goes Here> ");
+
     }
 
+
     public void onSayHelloClicked(View v) {
-        Toast.makeText(this, "Hello World", Toast.LENGTH_LONG).show();
+        text1.setText("Hello World!!");
     }
+
+
+
+
 }
